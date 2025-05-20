0c33901fc75d42c417a25e8b57838ec01069bb87
Jeremy Domingos <151725041+JeremyDomingos@users.noreply.github.com>
2025-05-20 18:03:12 +0200

    Add initial implementation of HTTP server in C

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # HTTP-Server
-A simple http server. 
+
+A simple http server.
+
+Clients make HTTP requests to servers.
+Servers respond to a client's HTTP request.
+
+1. When requesting a file via HTTP, clients must provide the file's URL.
+2. The web server must answer every HTTP request, at least with an error message.
+3. On a web server, the HTTP server is responsible for processing and answering incoming requests.
+4. Upon receiving a request, an HTTP server checks if the requested URL matches an existing file.
+
+   a- If so, the web server sends the file content back to the browser. If not, the server will check if it should generate a file dynamically for the request (see Static vs. dynamic content).
+
+   b- If neither of these options are possible, the web server returns an error message to the browser, most commonly 404 Not Found. The 404 error is so common that some web designers devote considerable time and effort to designing 404 error pages.
+
+SOURCE: (https://developer.mozilla.org/en-US/docs/Learn_web_development/Howto/Web_mechanics/What_is_a_web_server)
---
+++ server.c
@@ -0,0 +1,165 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#define _WIN32_WINNT 0x0601
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+#define DEFAULT_PORT "9999"
+#define DEFAULT_BUFF_LEN 512
+
+int __cdecl main(void)
+{
+    // Fill in getaddrinfo struct
+
+    WSADATA wsaData;
+    int iResult;
+
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+    ZeroMemory(&hints, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+
+    if (iResult != 0)
+    {
+        printf("Cannot set addrinfo\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // Listening socket defined
+
+    SOCKET ListeningSocket = INVALID_SOCKET;
+
+    ListeningSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+    if (ListeningSocket == INVALID_SOCKET)
+    {
+        printf("Invaild Socket %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+
+        return 2;
+    }
+
+    // Bind the socket
+
+    iResult = bind(ListeningSocket, result->ai_addr, (int)result->ai_addrlen);
+
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("Cannot Bind %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListeningSocket);
+        WSACleanup();
+
+        return 3;
+    }
+
+    freeaddrinfo(result);
+
+    //
+
+    if (listen(ListeningSocket, SOMAXCONN) == SOCKET_ERROR)
+    {
+        printf("Cannot Listen %ld\n", WSAGetLastError());
+        closesocket(ListeningSocket);
+        WSACleanup();
+
+        return 4;
+    }
+
+    // ACCEPT A CONNECTION
+
+    SOCKET ClientSocket;
+
+    ClientSocket = INVALID_SOCKET;
+
+    ClientSocket = accept(ListeningSocket, NULL, NULL);
+
+    if (ClientSocket == INVALID_SOCKET)
+    {
+        printf("Cannot Accept %ld\n", WSAGetLastError());
+        closesocket(ClientSocket);
+        WSACleanup();
+
+        return 5;
+    }
+
+    closesocket(ListeningSocket);
+
+    char recvbuf[DEFAULT_BUFF_LEN];
+    int iSendResult;
+    int recvbuflen = DEFAULT_BUFF_LEN;
+
+    do
+    {
+        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+
+        if (iResult > 0)
+        {
+
+            iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+
+            if (iSendResult == SOCKET_ERROR)
+            {
+                printf("Cannot send %ld\n", WSAGetLastError());
+                closesocket(ClientSocket);
+                WSACleanup();
+
+                return 6;
+            }
+            else
+            {
+                printf("Bytes received: %d\n", iResult);
+            }
+        }
+        else if (iResult == 0)
+        {
+            printf("Closing Connection..........\n");
+        }
+        else
+        {
+            printf("Cannot recv/send %ld\n\r\n", WSAGetLastError());
+            closesocket(ClientSocket);
+            WSACleanup();
+
+            return 7;
+        }
+
+    } while (iResult > 0);
+
+    iResult = shutdown(ClientSocket, SD_SEND);
+
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("Invalid socket %ld", WSAGetLastError());
+        closesocket(ClientSocket);
+        WSACleanup();
+
+        return 8;
+    }
+    // CLEANUP
+
+    WSACleanup();
+    closesocket(ClientSocket);
+    return 0;
+}
